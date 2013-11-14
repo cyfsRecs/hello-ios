d23ef84a08d6690eb9e3d4655f0efa8c823c5fb0
Jason Weaver <indyplanets@gmail.com>
2013-11-14 03:16:05 -0600

    Initial Commit

---
+++ hello-ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  hello-ios
+//
+//  Created by Jason Weaver on 11/14/13.
+//  Copyright (c) 2013 Jason Weaver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ hello-ios/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  hello-ios
+//
+//  Created by Jason Weaver on 11/14/13.
+//  Copyright (c) 2013 Jason Weaver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
